b0b9e468884bcc0a233d303a47de8356cc345d10
oya riku <>
2020-12-10 00:36:21 +0900

    add: パート１完了

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
-const { ApolloServer } = require("applo-server");
-const typeDefs = require("./schema");
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
 
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
