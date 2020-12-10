f452dbc1b5db49f309f8d699a0c3b3d22fa272a5
oya riku <riku03290@gmail.com>
2020-12-10 23:15:24 +0900

    add:パート2の完了

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require("apollo-server");
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+const typeDefs = require("./schema");
+
+const store = createStore()
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
