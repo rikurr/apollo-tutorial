65d60ffdd4a106c58a5868f0026a11a558f694a9
oya riku <riku03290@gmail.com>
2020-12-12 11:18:06 +0900

    add:パート4完了

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -3,24 +3,14 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     launches( # replace the current launches query with this one.
-      """
-      The number of results to show. Must be >= 1. Default = 20
-      """
       pageSize: Int
-      """
-      If you add a cursor here, it will only return results _after_ this cursor
-      """
+
       after: String
     ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
   }
 
-  """
-  Simple wrapper around our list of launches that contains a cursor to the
-  last item in the list. Pass this cursor to the launches query to fetch results
-  after these.
-  """
   type LaunchConnection {
     cursor: String!
     hasMore: Boolean!
