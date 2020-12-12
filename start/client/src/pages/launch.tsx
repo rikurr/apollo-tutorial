81cbd69b968cdd57c9885452ef365a13d65a7a3c
oya riku <riku03290@gmail.com>
2020-12-12 23:28:51 +0900

    add:パート6完了

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery } from "@apollo/client";
+import { LAUNCH_TILE_DATA } from "./launches";
+import { Header, LaunchDetail, Loading } from "../components";
+import { ActionButton } from "../containers";
 
-interface LaunchProps extends RouteComponentProps {
+interface LaunchProps extends RouteComponentProps {}
 
-}
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      site
+      rocket {
+        type
+      }
+      ...LaunchTile
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
-}
+const Launch: React.FC<LaunchProps> = ({ launchId }: any) => {
+  const { data, loading, error } = useQuery(GET_LAUNCH_DETAILS, {
+    variables: { launchId },
+  });
+  if (loading) return <Loading />;
+  if (!data || error) return <p>Error</p>;
+  return (
+    <Fragment>
+      <Header image={data.launch.mission.missionPatch}>
+        {data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
+};
 
 export default Launch;
--- start/client/src/pages/profile.tsx
+++ start/client/src/pages/profile.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery } from "@apollo/client";
+
+import { Loading, Header, LaunchTile } from "../components";
+import { LAUNCH_TILE_DATA } from "./launches";
 
 interface ProfileProps extends RouteComponentProps {}
 
+const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 const Profile: React.FC<ProfileProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery(GET_MY_TRIPS, {
+    fetchPolicy: "network-only",
+  });
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.tirps.length ? (
+        data.me.trips.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
+};
 
 export default Profile;
